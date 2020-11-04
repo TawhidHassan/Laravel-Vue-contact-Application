007adf9c0506c72905926b0ba2e623945556e505
sifat007 <sifat27.ht@gmail.com>
2020-11-04 09:28:54 +0600

    install progress bar nad configer

--- resources/js/app.js
+++ resources/js/app.js
@@ -16,6 +16,13 @@ window.Form=Form;
 Vue.component(HasError.name, HasError)
 Vue.component(AlertError.name, AlertError)
 
+import VueProgressBar from 'vue-progressbar'
+Vue.use(VueProgressBar, {
+    color: 'rgb(143, 255, 199)',
+    failedColor: 'red',
+    height: '3px'
+  })
+
 
 import VueRouter from 'vue-router'
 Vue.use(VueRouter)
